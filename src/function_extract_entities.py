d7439fbf17363311eca43147ac941c8a3804eff8
MJ Schanne <mjschanne@microsoft.com>
2024-03-14 10:03:52 -0400

    Add functionality to function_extract_entities.py

--- src/function_extract_entities.py
+++ src/function_extract_entities.py
@@ -1,10 +1,32 @@
 import azure.durable_functions as df
-import logging
+from langchain_core.documents import Document
+from kernel_factory import KernelFactory
+import semantic_kernel as sk
 
 extract_entities_blueprint = df.Blueprint()
 
 
 @extract_entities_blueprint.activity_trigger(input_name="chunks")
-def extract_entities(chunks: str) -> str:
-    logging.info(f"Called extract_entities_blueprint on {chunks}!")
-    return chunks
+async def extract_entities(chunk: Document) -> dict:
+    kernel = KernelFactory.create_kernel()
+
+    multi_entity_result = await kernel.invoke(
+        kernel.plugins["EntityExtraction"]["ExtractMultipleEntities"],
+        sk.KernelArguments(input=chunk),
+    )
+
+    stakeholders_result = await kernel.invoke(
+        kernel.plugins["EntityExtraction"]["ExtractStakeholders"],
+        sk.KernelArguments(input=chunk),
+    )
+
+    dates_result = await kernel.invoke(
+        kernel.plugins["EntityExtraction"]["ExtractSignificantDates"],
+        sk.KernelArguments(input=chunk),
+    )
+
+    return {
+        "multi_entity_result": multi_entity_result.value[0].content,
+        "stakeholders_result": stakeholders_result.value[0].content,
+        "dates_result": dates_result.value[0].content,
+    }
