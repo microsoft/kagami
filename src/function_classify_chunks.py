1d8f8549d8ca29bd7bd6d5af80abdba795b0c5cd
MJ Schanne <mjschanne@microsoft.com>
2024-03-14 09:11:57 -0400

    Add functionality to function_classify_chunks.py

--- src/function_classify_chunks.py
+++ src/function_classify_chunks.py
@@ -1,10 +1,27 @@
 import azure.durable_functions as df
-import logging
+from langchain_core.documents import Document
+import json, os
+from kernel_factory import KernelFactory
+import semantic_kernel as sk
+from typing import List
 
 classify_chunks_blueprint = df.Blueprint()
 
 
 @classify_chunks_blueprint.activity_trigger(input_name="chunks")
-def classify_chunks(chunks: str) -> str:
-    logging.info(f"Called classify_chunks on {chunks}!")
-    return chunks
+async def classify_chunks(chunks: List[Document]) -> str:
+    kernel = KernelFactory.create_kernel()
+
+    # for larger documents the chunk size maybe too large for the plugin to handle simultaneously
+    joined_chunks = f"{os.linesep}".join(chunks)
+
+    skresult = await kernel.invoke(
+        kernel.plugins["ClassificationPlugin"]["ClassifyStudyType"],
+        sk.KernelArguments(input=joined_chunks),
+    )
+
+    # Attempt to parse the JSON string
+    try:
+        return json.loads(skresult.value[0].content)
+    except json.JSONDecodeError:
+        return False
