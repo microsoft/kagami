513aea8884119001ad023d089bd30a17d751406e
cajetzer <64230564+cajetzer@users.noreply.github.com>
2024-03-17 22:24:54 -0400

    add deployment settings file support and instructions

--- powerplat/export-solution.ps1
+++ powerplat/export-solution.ps1
@@ -13,6 +13,10 @@ $managedPath = $settings.solutionDir + $settings.solutionName + "-" + $settings.
 # Export solution files, save to project, increment version in source environment
 pac solution export --path $unmanagedPath --name $settings.solutionName --managed false
 pac solution export --path $managedPath --name $settings.solutionName --managed true
+# increment solution version in source environment
 pac solution online-version --solution-name $settings.solutionName --solution-version $settings.newSolutionVersion
+# create settings file to set connection references
+$deploymentSettingsPath = $settings.solutionDir + $settings.deploymentSettings
+pac solution create-settings --solution-zip $managedPath --settings-file $deploymentSettingsPath
 # unpack solution into source
 pac solution unpack --zipfile $unmanagedPath --folder $settings.srcDir
